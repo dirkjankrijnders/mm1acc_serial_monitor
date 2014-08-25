a9a77d9eeb485f293b42bb6b10275391bd92775b
Dirkjan Krijnders <dirkjankrijnders@incas3.eu>
2014-08-25 16:25:54 +0200

    Quick implementation

--- firmware/main.c
+++ firmware/main.c
@@ -5,12 +5,25 @@
  */
 
 #include <avr/io.h>
+#include <stdio.h>
+
+#include <mm1acc.h>
+#include <uart.h>
 
 int main(void)
 {
+	acc_data data;
+	char buf[16];
+	mm1acc_init();
+	uart_init(UART_BAUD_SELECT(76800, F_CPU));
+	uart_puts("mm1acc serial monitor");
     /* insert your hardware initialization here */
     for(;;){
         /* insert your main loop code here */
+		if (mm1acc_check(&data)) {
+			sprintf(buf, "M0x%.2x - 0x%.2x %x", data.address, data.port, data.function);
+			uart_puts(buf);
+		}
     }
     return 0;   /* never reached */
 }
